a6709a6a09d32c7e96dcc7f45615d6afa7c768af
Adi Firman Ilham <firman_bjs@ymail.com>
2020-07-09 09:31:15 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Conditional from "./components/Conditional";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+      });
+    }, 1500);
+  }
+
+  render() {
+    return (
+      <div>
+        <Conditional isLoading={this.state.isLoading} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ components/Conditional.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Conditional(props) {
+  console.log(props.isLoading);
+  return <h1>Hello World!</h1>;
+}
+
+export default Conditional;
