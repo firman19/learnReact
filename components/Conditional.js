784441d4ef1d7ddddd296e857633be0d24053c6c
firman19 <firman_bjs@ymail.com>
2020-07-09 13:44:02 +0700

    add exclamation mark

--- components/Conditional.js
+++ components/Conditional.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Conditional(props) {
   console.log(props.isLoading);
-  return <h1>Hello World!</h1>;
+  return <h1>Hello World!!!</h1>;
 }
 
 export default Conditional;
